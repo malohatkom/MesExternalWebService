67617f1f05f200f705cb9467fd777159aa004eea
malohatkom <79734294+malohatkom@users.noreply.github.com>
2021-03-14 09:39:35 +0500

    Delete DecompilledJava directory

--- DecompilledJava/src/mesclient/businessservice/internalData/DataTableComparator$IColumnComparator.java
+++
@@ -1,7 +0,0 @@
-package de.mpdv.mesclient.businessservice.internalData;
-
-
-interface DataTableComparator$IColumnComparator {
-
-   int compare(Object var1, Object var2, Class var3);
-}
--- DecompilledJava/src/mesclient/businessservice/package-info.java
+++
@@ -1,9 +0,0 @@
-package de.mpdv.mesclient.businessservice;
-
-import javax.xml.bind.annotation.XmlSchema;
-
-@XmlSchema(
-   namespace = "http://businessService.mesClient.mpdv.de/"
-)
-interface package-info {
-}
